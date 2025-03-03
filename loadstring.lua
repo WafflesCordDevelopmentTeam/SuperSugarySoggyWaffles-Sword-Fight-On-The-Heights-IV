82c68ab043dd0ace396710332d8a5f2b639f4503
Waffles <w4fflessa@gmail.com>
2025-03-03 04:13:21 -0600

    Add files via upload
    loadstring

---
+++ loadstring.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/WafflesCordDevelopmentTeam/SuperSugarySoggyWaffles-Sword-Fight-On-The-Heights-IV/refs/heads/main/SugarWaffle%27s%20SFOTH.lua'))()
